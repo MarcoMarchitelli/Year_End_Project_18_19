195dfdb8edcc4c92df0f359612ca4e8dc8fbfe1c
Michele Pesante <pesante.michele.96@gmail.com>
2019-01-09 17:26:47 +0100

    Creato prefab debugger (Con esc si vedono i tasti di debug)

---
+++ Assets/Scripts/DebugController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour
+{
+    private Canvas myCanvas;
+
+    private void Start()
+    {
+        myCanvas = GetComponentInChildren<Canvas>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SwitchCanvasStatus();
+        }
+    }
+
+    private void SwitchCanvasStatus()
+    {
+        myCanvas.enabled = !myCanvas.enabled;
+        Time.timeScale = myCanvas.enabled ? 0 : 1;
+    }
+}
