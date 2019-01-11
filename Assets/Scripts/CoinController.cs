118bab2f2419275537ed2e201ebfd2d7cc2b7fa7
MrGrox <g.ferrari96@gmail.com>
2019-01-11 17:20:50 +0100

    Creato prefab moneta, da modificare in futuro, sono Michele

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    private Rigidbody2D myRb;
+    private CoinManager myCoinManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+        myCoinManager = FindObjectOfType<CoinManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        myCoinManager.ChangeScore(1);
+        Destroy(gameObject);
+    }
+}
