98d5a6eb2724254937bea9d5f2da378018ce7375
senar93 <andre.teru93@gmail.com>
2019-01-21 09:03:16 +0100

    - Reinserito Cinemachine per la gestione della camera - aggiunto uno script per selezionare automaticamente il target della camera, il primo oggetto taggato CameraTarget - Creato il prefab della Camera e modificato il prefab del player
    - riorganizzati alcuni prefab in sottocartelle della cartella prefabs

---
+++ Assets/Scripts/Camera/FindCameraTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FindCameraTarget : MonoBehaviour
+{
+
+	private string tagName = "CameraTarget";
+
+	// Start is called before the first frame update
+	void Awake()
+    {
+		GameObject tmpPointer = GameObject.FindGameObjectWithTag(tagName);
+		if(tmpPointer) {
+			GetComponent<CinemachineVirtualCamera>().Follow = tmpPointer.transform;
+		}
+	}
+
+}
