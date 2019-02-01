b90f221aeb8ee8ccf5288e4e39df302feb328fb2
senar93 <andre.teru93@gmail.com>
2019-02-01 18:06:54 +0100

    - spostato il prefab del player (contenente anche la logica) dalla cartella Graphic alla cartella Prefabs - aggiunta la state machine che gestisce il movimento orizzontale del player (con anche la logica), questa state machine varia la posizione del target che la camera deve seguire, è possibile modificarne la posizione selezionando lo stato nel layer "Camera Horizontal ..." - aggiunta la state machine che gestisce il movimento verticale del player (senza logica)

---
+++ Assets/Scripts/Camera/State Machine/CameraHorizontalFacingState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraHorizontalFacingState : CameraState
+{
+
+	public AnimationCurve transitionAnimation;
+	public float xPosition;
+
+	protected Transform cameraTarget;
+	protected float timer;
+	protected Vector3 actCameraTargetPosition;
+
+	private float maxEvaluationTimer = 5f;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		cameraTarget = mainVcam.Follow;
+	}
+
+	protected override void Enter()
+	{
+		timer = 0;
+		actCameraTargetPosition = cameraTarget.localPosition;
+	}
+
+	protected override void Tick()
+	{
+		if (timer < maxEvaluationTimer)
+		{
+			timer += Time.deltaTime;
+			actCameraTargetPosition.x = Mathf.Lerp(cameraTarget.localPosition.x, xPosition, transitionAnimation.Evaluate(timer));
+			actCameraTargetPosition.y = cameraTarget.localPosition.y;
+			actCameraTargetPosition.z = cameraTarget.localPosition.z;
+			cameraTarget.localPosition = actCameraTargetPosition;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Camera/State Machine/CameraState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public abstract class CameraState : PlayerStateBase
+{
+
+	protected CinemachineVirtualCamera mainVcam;
+
+	protected virtual void Awake()
+	{
+		mainVcam = GameObject.FindGameObjectWithTag("MainVcam").GetComponent<CinemachineVirtualCamera>();
+	}
+
+}
