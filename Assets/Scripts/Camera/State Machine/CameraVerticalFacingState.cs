d638a5e6b8ffd48c5d6404d415a2ec7e8415c338
senar93 <andre.teru93@gmail.com>
2019-02-04 14:48:59 +0100

    - inserito il movimento della camera verticale, con la stessa logica del movimento orizzontale, attualmente abilitato solo per lo stato Rising, con Falling si generava un effetto strano TO DO: - sistemare i valori del layer "Camera Vertical Routing" per gestire i movimenti extra della camera per il salto e la caduta

---
+++ Assets/Scripts/Camera/State Machine/CameraVerticalFacingState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVerticalFacingState : CameraState
+{
+	public AnimationCurve transitionAnimation;
+	public float yPosition;
+
+	protected Transform cameraTarget;
+	protected float timer;
+	protected Vector3 actCameraTargetPosition;
+
+	private float maxEvaluationTimer = 5f;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		cameraTarget = mainVcam.Follow;
+	}
+
+	protected override void Enter()
+	{
+		timer = 0;
+		actCameraTargetPosition = cameraTarget.localPosition;
+	}
+
+	protected override void Tick()
+	{
+		if (timer < maxEvaluationTimer)
+		{
+			timer += Time.deltaTime;
+			actCameraTargetPosition.x = cameraTarget.localPosition.x;
+			actCameraTargetPosition.y = Mathf.Lerp(cameraTarget.localPosition.y, yPosition, transitionAnimation.Evaluate(timer));
+			actCameraTargetPosition.z = cameraTarget.localPosition.z;
+			cameraTarget.localPosition = actCameraTargetPosition;
+		}
+	}
+}
