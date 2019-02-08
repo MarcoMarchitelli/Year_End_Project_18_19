886e6db2cb892a8539b6edfba9621ffa224139c5
Firada <lori.scillons@gmail.com>
2019-02-08 15:57:52 +0100

    aggiunto fix material nemico

---
+++ Assets/Scripts/FIX_MaterialEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FIX_MaterialEnemy : MonoBehaviour
+{
+
+    public Material mat;
+    public GameObject graphic;
+
+    private void Awake()
+    {
+        foreach(MeshRenderer mr in graphic.GetComponentsInChildren<MeshRenderer>(){
+            mr.material = mat;
+        }
+    }
+
+
+
+}
