77eada3d392fb8a62681aba1148d2667ec9c8166
Marco Marchitelli <marchitelli97@libero.it>
2019-05-07 20:55:56 +0200

    Useless animation wire up.

---
+++ Assets/God Hunt/Enemies/Useless/Scripts/UselessAnimatorProxy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class UselessAnimatorProxy : MonoBehaviour
+{
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    bool _isWalking;
+    public bool IsWalking
+    {
+        get { return _isWalking; }
+        set
+        {
+            if (value != _isWalking)
+            {
+                _isWalking = value;
+                animator.SetBool("IsWalking", _isWalking);
+            }
+        }
+    }
+
+    public void Death()
+    {
+        animator.SetTrigger("Death");
+    }
+
+}
