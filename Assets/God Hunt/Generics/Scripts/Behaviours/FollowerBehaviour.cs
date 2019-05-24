89c1a1e71cef4d841a6f3ee8534d65360a891e70
Lorenzo Scillone <lori.scillons@gmail.com>
2019-05-24 12:46:40 +0200

    Started shooting system implementation.

---
+++ Assets/God Hunt/Generics/Scripts/Behaviours/FollowerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class FollowerBehaviour : BaseBehaviour
+{
+    public float moveSpeed;
+    public float turnSpeed;
+    public bool startFollowingOnAwake = true;
+
+    Transform target;
+    float targetAngle, angle;
+    Vector3 directionToTarget;
+
+    bool follow = false;
+
+    protected override void CustomSetup()
+    {
+        PlayerEntity player = FindObjectOfType<PlayerEntity>();
+        if (player)
+        {
+            //player.OnDeath += EndFollow;
+            target = player.transform;
+        }
+
+        if(startFollowingOnAwake)
+            StartFollow();
+    }
+
+    void Update()
+    {
+        if (!follow)
+            return;
+
+        //go towards target
+        directionToTarget = (target.position - transform.position).normalized;
+        transform.Translate(-directionToTarget * moveSpeed * Time.deltaTime);
+
+        //face target
+        targetAngle = 90 - Mathf.Atan2(directionToTarget.z, directionToTarget.x) * Mathf.Rad2Deg;
+        angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, Time.deltaTime * turnSpeed);
+        transform.eulerAngles = Vector3.up * angle;
+    }
+
+    public void StartFollow()
+    {
+        follow = true;
+    }
+
+    public void EndFollow()
+    {
+        follow = false;
+    }
+}
---
+++ Assets/God Hunt/Generics/Scripts/Behaviours/GunBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class GunBehaviour : BaseBehaviour
+{
+    public Projectile projectilePrefab;
+    public Transform shootPoint;
+    public float timeBetweenEachShot = .5f;
+    public float projectileSpeed = 25f;
+    public Vector3 projectileRotation;
+    public bool startShootingOnAwake = true;
+
+    float timer = 0;
+    bool canShoot = true;
+
+    protected override void CustomSetup()
+    {
+        if (startShootingOnAwake)
+        {
+            StartShooting();
+        }
+    }
+
+    private void Update()
+    {
+        if (canShoot)
+            timer -= Time.deltaTime;
+        else
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeBetweenEachShot)
+                timer = 0;
+        }
+    }
+
+    public void Shoot()
+    {
+        if (!canShoot)
+            return;
+
+        if (timer <= 0)
+        {
+            Projectile proj = Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation) as Projectile;
+            proj.moveSpeed = projectileSpeed;
+            proj.transform.rotation = proj.transform.rotation * Quaternion.Euler(projectileRotation);
+            timer = timeBetweenEachShot;
+        }
+    }
+
+    public void StartShooting()
+    {
+        canShoot = true;
+    }
+
+    public void EndShooting()
+    {
+        canShoot = false;
+    }
+}
