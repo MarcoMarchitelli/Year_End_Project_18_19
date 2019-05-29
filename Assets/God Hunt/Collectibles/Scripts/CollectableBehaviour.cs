16e4fc08fa218bf19287d2016bc35dd29abb30cb
Marco Marchitelli <marchitelli97@libero.it>
2019-05-29 22:08:03 +0200

    Added collectable behaviour. Reverted the player prefab to the previous collectable system.

---
+++ Assets/God Hunt/Collectibles/Scripts/CollectableBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CollectableBehaviour : BaseBehaviour
+{
+    [SerializeField] UnityVoidEvent OnCollection;
+
+    PlayerEntityData playerData;
+
+    protected override void CustomSetup()
+    {
+        playerData = GameManager.Instance.player.Data as PlayerEntityData;
+    }
+
+    public void Collect()
+    {
+        playerData.playerCollectablesBehaviour.Collect();
+        OnCollection.Invoke();
+    }
+}
