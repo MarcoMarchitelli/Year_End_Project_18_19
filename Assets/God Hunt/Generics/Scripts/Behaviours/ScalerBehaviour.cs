3e8a5ee5c69f5fb9e90b0bd7ac82985fbafecb96
MarcoMarchitelli <igvvcdhk@gmail.com>
2019-05-31 11:33:43 +0200

    Added scaler and teleport behaviours.

---
+++ Assets/God Hunt/Generics/Scripts/Behaviours/ScalerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class ScalerBehaviour : BaseBehaviour
+{
+    public Vector3 startScale;
+    public Vector3 endScale;
+    public float duration;
+    public UnityVoidEvent OnScaleStart, OnScaleEnd;
+
+    public void ScaleThisBehaviour()
+    {
+        OnScaleStart.Invoke();
+        transform.localScale = startScale;
+        transform.DOScale(endScale, duration).onComplete += () => OnScaleEnd.Invoke();
+    }
+
+    public void ScaleEntity()
+    {
+        OnScaleStart.Invoke();
+        Entity.gameObject.transform.localScale = startScale;
+        Entity.gameObject.transform.DOScale(endScale, duration).onComplete += () => OnScaleEnd.Invoke();
+    }
+}
---
+++ Assets/God Hunt/Generics/Scripts/Behaviours/TeleportBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TeleportBehaviour : BaseBehaviour
+{
+    public Transform[] teleportPoints;
+    public UnityVoidEvent OnTeleport;
+
+    public void TeleportThisBehaviour(int _teleportPointIndex)
+    {
+        transform.position = teleportPoints[_teleportPointIndex].position;
+        OnTeleport.Invoke();
+    }
+
+    public void TeleportThisEntity(int _teleportPointIndex)
+    {
+        Entity.gameObject.transform.position = teleportPoints[_teleportPointIndex].position;
+        OnTeleport.Invoke();
+    }
+}
