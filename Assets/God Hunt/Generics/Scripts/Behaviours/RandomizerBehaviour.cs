739694dc1952af07ab48c74a00650681388dca91
Marco Marchitelli <marchitelli97@libero.it>
2019-06-06 20:37:32 +0200

    Added Randomizer behaviour.

---
+++ Assets/God Hunt/Generics/Scripts/Behaviours/RandomizerBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class RandomizerBehaviour : BaseBehaviour
+{
+    public Outcome[] possibleOutcomes;
+
+    public void Randomize()
+    {
+        float rng = Random.value;
+        float currentProb = 0;
+        foreach (Outcome outcome in possibleOutcomes)
+        {
+            currentProb += outcome.percentage;
+            if (rng <= currentProb)
+            {
+                outcome.onOutcomeRolled.Invoke();
+                break;
+            }
+        }
+    }
+
+    [ContextMenu("Test Randomize")]
+    public void RandomizeTest()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            float rng = Random.value;
+            float currentProb = 0;
+            foreach (Outcome outcome in possibleOutcomes)
+            {
+                currentProb += outcome.percentage;
+                if (rng <= currentProb)
+                {
+                    outcome.onOutcomeRolled.Invoke();
+                    print(outcome.description);
+                    break;
+                }
+            } 
+        }
+    }
+
+    [System.Serializable]
+    public class Outcome
+    {
+        [Multiline]
+        public string description;
+        [Range(0, 1)]
+        public float percentage;
+        public UnityVoidEvent onOutcomeRolled;
+    }
+}
