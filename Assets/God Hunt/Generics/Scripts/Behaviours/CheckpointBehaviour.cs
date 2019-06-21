7e68a881732e05c97fc07f0cd3e84486f2e02d26
MarcoMarchitelli <igvvcdhk@gmail.com>
2019-06-21 16:36:01 +0200

    Added the new checkpoint behaviour to the altar prefab!

---
+++ Assets/God Hunt/Generics/Scripts/Behaviours/CheckpointBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CheckpointBehaviour : BaseBehaviour
+{
+    PlayerEntityData playerData;
+
+    protected override void CustomSetup()
+    {
+        playerData = GameManager.Instance.player.Data as PlayerEntityData;
+    }
+
+    /// <summary>
+    /// Sets the player's respawn point as this object position.
+    /// </summary>
+    public void SetCheckpointAsThis()
+    {
+        playerData.respawnBehaviour.SetRespawnPoint(transform.position);
+    }
+
+    public void SetCheckpoint(Transform _transform)
+    {
+        playerData.respawnBehaviour.SetRespawnPoint(_transform.position);
+    }
+}
